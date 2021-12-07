dac1fe2b91422e0c68038b32d12fab7234aa4a9d
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2021-12-06 21:47:19 -0300

    ex 9 - calculadora divisão e multiplicação

---
+++ 9-calculadora.js
@@ -0,0 +1,18 @@
+// 9. Criar calculadora que o usuário coloca o valor e pode escolher se multiplica ou divide valor
+// e gera o resultado.
+
+let valorUm = document.querySelector("#valor1");
+let valorDois = document.querySelector("#valor2");
+let multiplicar = document.querySelector("#multiplicar");
+let dividir = document.querySelector("#dividir");
+let resultado = document.querySelector("#resposta");
+
+multiplicar.addEventListener("click", (e) => {
+  e.preventDefault();
+  resultado.innerHTML = valorUm.value * valorDois.value;
+});
+
+dividir.addEventListener("click", (e) => {
+  e.preventDefault();
+  resultado.innerHTML = valorUm.value / valorDois.value;
+});
