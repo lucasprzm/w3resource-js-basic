24afc7713354dbdb0d4481626a0f1c96abf7daca
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2021-12-13 15:06:06 -0300

    Ex. 41 - três inteiros

---
+++ Exercicios_41-50/41-tresIguais.js
@@ -0,0 +1,15 @@
+// 41. Programa que verifica se os três números são iguais (retorna o valor 30), se 2 são iguais (retorna o valor 40) e se todos são diferentes (retorna o valor 20).
+function tresValores(x, y, z) {
+  if (x == y && x == z) {
+    console.log(30);
+  } else if ((x == y && x != z) || (x == z && x != y) || (y == z && y != x)) {
+    console.log(40);
+  } else {
+    console.log(20);
+  }
+}
+tresValores(10, 10, 10);
+tresValores(15, 15, 20);
+tresValores(20, 15, 20);
+tresValores(20, 25, 25);
+tresValores(30, 35, 40);
