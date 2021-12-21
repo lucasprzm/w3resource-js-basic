92071ae46a16b77e3ea1275ee74b0d64c785a4c9
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2021-12-21 18:58:42 -0300

    alteração ex. 67

--- Exercicios_61-70/67-stringP.js
+++ Exercicios_61-70/67-stringP.js
@@ -1,12 +1,19 @@
 // 67. Função que verifica se tem a letra P no início ou no fim da string, se tiver, retira o primeiro e último caracteres da string. Caso não tenha, retorna a string normalmente.
 function removeP(string) {
   if (
-    string.indexOf("P") == 0 ||
-    string.indexOf("P") == string.length - 1 ||
-    string.indexOf("p") == 0 ||
-    string.indexOf("p") == string.length - 1
+    (string.indexOf("P") == 0 && string.indexOf("P") == string.length - 1) ||
+    (string.indexOf("p") == 0 && string.indexOf("p") == string.length - 1) ||
+    (string.indexOf("P") == 0 && string.indexOf("p") == string.length - 1) ||
+    (string.indexOf("p") == 0 && string.indexOf("P") == string.length - 1)
   ) {
     console.log(string.substring(1, string.length - 1));
+  } else if (string.indexOf("P") == 0 || string.indexOf("p") == 0) {
+    console.log(string.substring(1));
+  } else if (
+    string.indexOf("P") == string.length - 1 ||
+    string.indexOf("p") == string.length - 1
+  ) {
+    console.log(string.substring(0, string.length - 1));
   } else {
     console.log(string);
   }
@@ -15,3 +22,4 @@ removeP("Python");
 removeP("eduardo");
 removeP("Pedro");
 removeP("pedro");
+removeP("eduardop");
