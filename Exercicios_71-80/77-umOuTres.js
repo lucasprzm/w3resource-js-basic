a23e0e8f2fa4de5b25fb51b40f54d5cc01f6e124
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2021-12-28 12:12:15 -0300

    77 e 78 - 1 e 3 no array.

---
+++ Exercicios_71-80/77-umOuTres.js
@@ -0,0 +1,7 @@
+// 77. Função que verifica se tem os números 1 ou 3 em um determinado array de 2 elementos.
+function umTres(array) {
+  console.log(array.indexOf(1) != -1 || array.indexOf(3) != -1);
+}
+umTres([1, 5]);
+umTres([2, 3]);
+umTres([2, 4]);
